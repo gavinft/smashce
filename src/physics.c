de42c15042d3c7cbfd9beaae153aa86cf8535069
gIsForGravity <gavin.f.tantleff@gmail.com>
2026-01-18 01:34:18 -0500

    basic collision added

--- src/physics.c
+++ src/physics.c
@@ -1,3 +1,52 @@
+
+#include <stdint.h>
 #include "physics.h"
 
-collider_t* phy_colliders[16];
+collider_t* phy_colliders[PHY_COLLIDERS_LEN] = {0};
+rb_t* phy_rbs[PHY_RBS_LEN] = {0};
+
+void phy_step(float dt) {
+    for (int i = 0; i < PHY_RBS_LEN; i++) { // TODO: optimize loop (short circuit entire loop)
+        if (!phy_rbs[i])
+            continue;
+        rb_t* rb = phy_rbs + i;
+        
+        rb->vel.y += PHY_GRAVITY;
+        for (int i = 0; i < PHY_COLLIDERS_LEN; i++) {
+            if (!phy_colliders[i])
+                continue;
+            if (phy_colliders[i] == &rb->col)
+                continue;
+            
+            collider_t* this = &rb->col;
+            collider_t* that = phy_colliders + i;
+
+            if (that->layer & phy_layer_player)
+                continue;
+            if (!phy_col_overlap(*this, *that))
+                continue;
+            
+            // Overlap handling
+            if (phy_col_bot(*this) > phy_col_top(*that)) {
+                if (rb->vel.y <= 0)
+                    rb->vel.y = 0;
+                this->pos.y = phy_col_top(*that) + this->extent.y;
+            }
+            if (phy_col_top(*this) < phy_col_bot(*that)) {
+                if (rb->vel.y >= 0)
+                    rb->vel.y = 0;
+                this->pos.y = phy_col_top(*that) - this->extent.y;
+            }
+            if (phy_col_left(*this) < phy_col_right(*that)) {
+                if (rb->vel.x <= 0)
+                    rb->vel.x = 0;
+                this->pos.x = phy_col_top(*that) + this->extent.x;
+            }
+            if (phy_col_right(*this) > phy_col_right(*that)) {
+                if (rb->vel.x >= 0)
+                    rb->vel.x = 0;
+                this->pos.x = phy_col_top(*that) - this->extent.x;
+            }
+        }
+    }
+}
--- src/physics.h
+++ src/physics.h
@@ -1,18 +1,40 @@
 #ifndef PHYSICS_H
 #define PHYSICS_H
 
+#include <stdint.h>
 #include "vec.h"
+
+#define PHY_GRAVITY 1
 
 typedef struct {
     vec2_t pos; /* center of collider */
-    vec2_t size; /* x-width and y-height */
+    vec2_t extent; /* x-width and y-height */
+    uint8_t layer;
 } collider_t;
 
-extern collider_t* phy_colliders[16];
+#define phy_layer_player (1 << 0)
+#define phy_layer_stage (1 << 1)
+
+#define phy_col_bot(col) ((col).pos.y + (col).extent.y)
+#define phy_col_top(col) ((col).pos.y - (col).extent.y)
+#define phy_col_left(col) ((col).pos.x - (col).extent.x)
+#define phy_col_right(col) ((col).pos.x + (col).extent.x)
+#define phy_col_overlap(col1, col2) (phy_col_bot(col1) > phy_col_top(col2) && \
+                                     phy_col_top(col1) < phy_col_bot(col2) && \
+                                     phy_col_left(col1) < phy_col_right(col2) && \
+                                     phy_col_right(col1) < phy_col_left(col2))
+
+#define PHY_COLLIDERS_LEN (4)
+extern collider_t* phy_colliders[PHY_COLLIDERS_LEN];
 
 typedef struct {
     collider_t col;
     vec2_t vel;
 } rb_t;
+
+#define PHY_RBS_LEN (4)
+extern rb_t* phy_rbs[PHY_RBS_LEN];
+
+void phy_step(float dt);
 
 #endif /* PHYSICS_H */
